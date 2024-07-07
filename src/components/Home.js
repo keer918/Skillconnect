8f2dfef0069b7bc4b2372c21e5b5e0887e27025f
keer918 <keerthana.gopi918@gmail.com>
2024-07-07 11:27:08 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,7 +54,7 @@ const redirect=()=>{
                     delay: 0.3,
                     ease: [0.5, 0.71, 1, 1.5],
                   }}
-                  initial={{ opacity: 0, scale: 0.5 }}
+                  initial={{ opacity: 0, scale: 1.5 }}
                   whileHover={{ scale: 1.2 }}
                   className="title"
                   style={{ color: "white" }}>
